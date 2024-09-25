588f95abe4ac998ae5b72e48b8f61cee7365df4d
SANTIAGOG03 <126630684+SANTIAGOG03@users.noreply.github.com>
2024-09-24 20:53:09 -0500

    Add files via upload

--- usersinfo.py
+++ usersinfo.py
@@ -1,19 +1,69 @@
-import requests,json
-class usersInfo:
+import requests
+import json
+
+
+
+class capture:
+
+    proveedor_2=[]
 
     def __init__(self):
-       self.listUsers:
+        self.dataJson=[]
 
-    def getUsers(self):
-        responseUsers = requests.get("https://datos.gov.co/resource/jtnk-dmga.json")
-        dataJson = responseUsers.json()
-        for ind in range(len(dataJson)):
-            print(dataJson[ind]['email_address'])
-    def validateUsers(self):
-        for email inf self.listUsers:
-prueba = usersInfo()
-prueba.getUsers()
+    def captura(self):
+        datos= requests.get('https://www.datos.gov.co/resource/m5pi-7cau.json')
+        self.dataJson = datos.json()
+
+    def limpieza(self):
+        for ind in range(len(self.dataJson)):
+            jsonClean = {
+                "Year": "",
+                "Quarter": "",
+                "Provider": "",
+                "Income": ""
+            }
+            if self.dataJson[ind]['proveedor'] == "ALMACENES EXITO INVERSIONES S.A.S.":
+                jsonClean['Provider'] = "MOVIL EXITO"
+            elif self.dataJson[ind]['proveedor'] == "AVANTEL S.A.S":
+                jsonClean['Provider'] = "WOM"
+            elif self.dataJson[ind]['proveedor'] == "VIRGIN MOBILE COLOMBIA S.A.S.":
+                jsonClean['Provider'] = "VIRGIN MOBILE"
+            elif self.dataJson[ind]['proveedor'] == "SUMA MOVIL S.A.S.":
+                jsonClean['Provider'] = "SUMA MOVIL"
+            elif self.dataJson[ind]['proveedor'] == "COLOMBIA MOVIL  S.A ESP":
+                jsonClean['Provider'] = "CLARO"
+            elif self.dataJson[ind]['proveedor'] == "LOGISTICA FLASH COLOMBIA S.A.S":
+                jsonClean['Provider'] = "FLASH"
+            elif self.dataJson[ind]['proveedor'] == "COMUNICACION CELULAR S A COMCEL S A":
+                jsonClean['Provider'] = "CLARO"
+            elif self.dataJson[ind]['proveedor'] == "EMPRESA DE TELECOMUNICACIONES DE BOGOTA S.A. ESP":
+                jsonClean['Provider'] = "ETB"
+            elif self.dataJson[ind]['proveedor'] == "SETROC MOBILE GROUP SAS":
+                jsonClean['Provider'] = "SETROC"
+            elif self.dataJson[ind]['proveedor'] == "PARTNERS TELECOM COLOMBIA SAS":
+                jsonClean['Provider'] = "PARTNERS"
+            elif self.dataJson[ind]['proveedor'] == "LIWA SAS ESP":
+                jsonClean['Provider'] = "LIWA"
+            elif self.dataJson[ind]['proveedor'] == "LOV TELECOMUNICACIONES SAS":
+                jsonClean['Provider'] = "LOV"
+            elif self.dataJson[ind]['proveedor'] == "UFF MOVIL SAS":
+                jsonClean['Provider'] = "UFF"
+            elif self.dataJson[ind]['proveedor'] == "COLOMBIA TELECOMUNICACIONES S.A. E.S.P.":
+                jsonClean['Provider'] = "MOVISTAR"
+            jsonClean['Year'] = self.dataJson[ind]['anno']
+            jsonClean['Quarter'] = self.dataJson[ind]['trimestre']
+            jsonClean['Income'] = self.dataJson[ind]['ingresos_por_mensajes']
+            print(jsonClean)
+
+    def savedata(self):
+        print("hello")
+
+    def impresion(self):
+        print("hello")
 
 
 
 
+prueba=capture()
+prueba.captura()
+prueba.limpieza()
